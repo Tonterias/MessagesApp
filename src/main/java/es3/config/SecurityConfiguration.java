cb058cf76cedca40eab283272045b7ff8be36757
Tonterias <ecorreos@htomail.com>
2018-05-27 12:17:26 +0200

    Commit 1

--- src/main/java/es3/config/SecurityConfiguration.java
+++ src/main/java/es3/config/SecurityConfiguration.java
@@ -132,6 +132,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .disable()
         .and()
             .authorizeRequests()
+            //.antMatchers("/api/**").permitAll()
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/authenticate").permitAll()
@@ -144,8 +145,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/v2/api-docs/**").permitAll()
             .antMatchers("/swagger-resources/configuration/ui").permitAll()
             .antMatchers("/swagger-ui/index.html").hasAuthority(AuthoritiesConstants.ADMIN)
-            .antMatchers(HttpMethod.GET, "/api/message").permitAll();
-
+            .antMatchers(HttpMethod.GET, "/api/messages").permitAll();
     }
 
 }
