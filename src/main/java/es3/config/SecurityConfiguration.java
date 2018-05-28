cdd9ed275aa6857d3d46a92f63431fe5031e1c83
Tonterias <ecorreos@htomail.com>
2018-05-28 18:28:20 +0200

    List of news Working

--- src/main/java/es3/config/SecurityConfiguration.java
+++ src/main/java/es3/config/SecurityConfiguration.java
@@ -139,7 +139,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/account/reset-password/finish").permitAll()
             .antMatchers("/api/profile-info").permitAll()
             .antMatchers(HttpMethod.GET, "/api/messages").permitAll()
-            .antMatchers("/api/**").authenticated()
+            .antMatchers("/api/**").permitAll()
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
             .antMatchers("/v2/api-docs/**").permitAll()
